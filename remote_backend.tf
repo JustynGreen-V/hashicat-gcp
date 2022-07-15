fa2c1d07d468e63417de9dde96eae49fd466408d
JustynGreen <justyn.green@versent.com.au>
2022-07-15 04:28:49 +0000

    Added the first label

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "Versent-CTO"
     workspaces {
       name = "hashicat-gcp"
